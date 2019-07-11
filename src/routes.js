ee4dd60a0b3003cf7e6e7459b1d1aef79a096584
Letícia Costa <lcosta@verios.com.br>
2019-07-11 20:42:25 -0300

    adicionando rotas privadas

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Redirect, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Login from './pages/LoginPage';
 
+
+// retornar o componente
+const RotaPrivada = (props) => {
+  // const { permition, ...props } = props;
+  // checar o token
+  if (localStorage.getItem('token') === null) {
+    return <Redirect to="/login" />;
+  }
+
+  return <Route {...props} />
+};
+
 const routes = () => {
   return (
     <Switch>
-      <Route path="/" component={Home} exact />
+      <RotaPrivada path="/" component={Home} exact />
       <Route path="/login" component={Login} />
       <Route path="*" component={NotFound} />
     </Switch>
